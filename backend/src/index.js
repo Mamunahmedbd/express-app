44fcb39044e7764687cae437fb589845a0d9ea3b
Mamun Ahmed <mamunahmedusa@gmai..com>
2024-05-08 15:57:11 +0600

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var dotenv_1 = require("dotenv");
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+var port = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+    console.log("hello world");
+    res.send("Express + TypeScript Server");
+});
+app.listen(port, function () {
+    console.log("Hello test");
+    console.log("[server]: Server is running at http://localhost:".concat(port));
+});
